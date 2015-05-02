f8d04faaca3926702c3cae1d097846ad205a2582
chokdee <ju.melzer@gmail.com>
2015-05-02 12:06:22 +0200

    .

--- app/src/androidTest/java/pl/mg6/agrtt/TestActivityTests.java
+++ app/src/androidTest/java/pl/mg6/agrtt/TestActivityTests.java
@@ -2,25 +2,26 @@ package pl.mg6.agrtt;
 
 import android.test.ActivityInstrumentationTestCase2;
 //import android.view.WindowManager;
+import android.view.WindowManager;
 import android.widget.EditText;
 
 import com.robotium.solo.Solo;
 
 public class TestActivityTests extends ActivityInstrumentationTestCase2<TestActivity> {
 
-    public void testCanEnterTextAndPressButton() {
+    public void _testCanEnterTextAndPressButton() {
         solo.enterText(0, "my login");
         solo.enterText(1, "my password");
         solo.clickOnText("New Button");
     }
 
     public void testCanFindViewsEnterTextAndPressButton() {
-//        getInstrumentation().runOnMainSync(new Runnable() {
-//            @Override
-//            public void run() {
-//                getActivity().getWindow().addFlags(WindowManager.LayoutParams.FLAG_DISMISS_KEYGUARD);
-//            }
-//        });
+        getInstrumentation().runOnMainSync(new Runnable() {
+            @Override
+            public void run() {
+                getActivity().getWindow().addFlags(WindowManager.LayoutParams.FLAG_DISMISS_KEYGUARD);
+            }
+        });
         solo.enterText((EditText) solo.getView(R.id.editText1), "my login");
         solo.enterText((EditText) solo.getView(R.id.editText2), "my password");
         solo.clickOnView(solo.getView(R.id.button));
